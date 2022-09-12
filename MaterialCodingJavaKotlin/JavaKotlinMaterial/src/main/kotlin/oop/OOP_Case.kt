84136507b5dd5056764b1e72e4c2327722e7cf74
algokelvin-373 <kelvinht373@gmail.com>
2022-09-12 23:19:43 +0700

    [Material][Java Kotlin] OOP Basic

---
+++ MaterialCodingJavaKotlin/JavaKotlinMaterial/src/main/kotlin/oop/OOP_Case.kt
@@ -0,0 +1,28 @@
+package oop
+
+fun main() {
+    // Define object class "Person"
+    val person = Person()
+
+    // Call function with object 'person'
+    person.setPerson("Kelvin H Tandrio", "Jakarta", 26)
+
+    // Call attribute to get data
+    println("Name     : ${person.name}")
+    println("Domicile : ${person.domicile}")
+    println("Age      : ${person.age}")
+
+}
+
+class Person {
+    var name = ""
+    var domicile = ""
+    var age = 0
+
+    fun setPerson(name: String, domicile: String, age: Int) {
+        this.name = name
+        this.domicile = domicile
+        this.age = age
+    }
+
+}
