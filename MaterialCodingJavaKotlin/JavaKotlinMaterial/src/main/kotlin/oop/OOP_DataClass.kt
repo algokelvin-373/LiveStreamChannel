55603b4b0d2fab9b04004bce633b6e9e36869605
algokelvin-373 <kelvinht373@gmail.com>
2022-09-13 23:29:58 +0700

    [Material][Java Kotlin] OOP Data Class

---
+++ MaterialCodingJavaKotlin/JavaKotlinMaterial/src/main/kotlin/oop/OOP_DataClass.kt
@@ -0,0 +1,17 @@
+package oop
+
+fun main() {
+    //Create variable to data attribute 'data person'
+    val name = "Kelvin Tandrio"
+    val age = 26
+
+    // Initialize 'DataPerson' to variable 'person'
+    val person = DataPerson(name, age)
+
+    println("Name ${person.name} - (${person.age})")
+}
+
+data class DataPerson (
+    val name: String,
+    val age: Int
+)
